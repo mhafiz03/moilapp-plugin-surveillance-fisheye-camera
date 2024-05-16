94663121789d7d2fa432f985cbb8c64397ade485
tak2hu <mhafizkusuma3@gmail.com>
2024-05-16 09:40:57 +0700

    change setup to start after add

--- controller.py
+++ controller.py
@@ -6,6 +6,36 @@ from .ui_setup import Ui_Setup
 
 MAX_MONITOR_INDEX = 8
 
+class GridManager:
+    def __init__(self):
+        self.slots = [None] * 8
+
+    def get_index_of_slot(self, element):
+        try:
+            index = self.slots.index(element)
+            return index + 1  
+        except ValueError:
+            return -1
+
+    def get_slot_by_index(self, index):
+        if 1 <= index <= len(self.slots):
+            return self.slots[index - 1]
+        return None
+
+    def get_empty_slots(self):
+        return [i + 1 for i, slot in enumerate(self.slots) if slot is None]
+
+    def get_used_slots(self):
+        return [i + 1 for i, slot in enumerate(self.slots) if slot is not None]
+
+    def set_slot(self, index, element):
+        if 1 <= index <= len(self.slots):
+            self.slots[index - 1] = element
+
+    def clear_slot(self, index):
+        if 1 <= index <= len(self.slots):
+            self.slots[index - 1] = None
+
 # for the setup dialog
 class SetupDialog(QtWidgets.QDialog):
     def __init__(self):
@@ -26,16 +56,29 @@ class SetupDialog(QtWidgets.QDialog):
     # Escape Key does not invoke closeEvent (to disconnect the signals and slots), so need to do it manually
     def keyPressEvent(self, event):
         if event.key() == QtCore.Qt.Key.Key_Escape:
-            self.close()
+            self.close_function()
         else:
             super().keyPressEvent(event)
 
     # need to disconnect the signals and slots else RuntimeError: wrapped C/C++ object of type QLabel has been deleted
     # because the QLabel's lifetime will be over when the Setup Dialog is closed but the previous slot and signal will still call it
     def closeEvent(self, event):
+        self.close_function()
+        super().closeEvent(event)
+
+    def close_function(self):
+        self.disconnect_signals()
+        self.reject()
+        self.close()
+
+    def accept_function(self):
+        self.disconnect_signals()
+        self.accept()
+        self.close()
+    
+    def disconnect_signals(self):
         self.result_signal.disconnect(self.result_slot)
         self.original_signal.disconnect(self.original_slot)
-        super().closeEvent(event)
 
 
 class Controller(QtWidgets.QWidget):
@@ -54,10 +97,10 @@ class Controller(QtWidgets.QWidget):
         self.ui.recordedButton.clicked.connect(self.recorded_clicked)
         self.ui.capturedButton.clicked.connect(self.captured_clicked)
 
-        self.model2idx = {}
-        self.idx2model = {}
+        self.grid_manager = GridManager()
+
         self.set_stylesheet()
-        self.cur_idx = 1
+
 
     # find every QPushButton, QLabel, QScrollArea, and Line, this works because this class is a subclass of QWidget
     def set_stylesheet(self):
@@ -68,48 +111,51 @@ class Controller(QtWidgets.QWidget):
         self.ui.line_2.setStyleSheet(self.model.style_line())
         self.ui.line_3.setStyleSheet(self.model.style_line())
 
-    # create new widget with ui_tile design and add it into the tile_layout
     def add_clicked(self):
-        n = self.cur_idx
-        label = getattr(self.ui, "displayLab%s" % n)
-        setup_button = getattr(self.ui, "setupButton%s" % n)
-        capture_button = getattr(self.ui, "captureButton%s" % n)
+        ui_idx = self.grid_manager.get_empty_slots()[0]
+        label, setup_button, capture_button, delete_button = self.get_monitor_ui_by_idx(ui_idx)
 
         # I have no idea how this works but I think the order of calling these is important
-        model_apps = ModelApps()
         # model_apps.create_moildev()
         # model_apps.create_image_original()
-        model_apps.update_file_config()
         source_type, cam_type, media_source, params_name = self.model.select_media_source()
-        model_apps.set_media_source(source_type, cam_type, media_source, params_name)
-        # model_apps.create_maps_fov() # no clue what this does
+        if media_source is not None:
+            model_apps = ModelApps()
+            model_apps.update_file_config()
+            model_apps.set_media_source(source_type, cam_type, media_source, params_name)
+            # model_apps.create_maps_fov() # no clue what this does
 
-        model_apps.image_result.connect(lambda img: self.update_label_image(img, label))
-        # the above is sufficient if wanting to display videos, but the below one is needed to display images
-        self.update_label_image(model_apps.image, label)
+            return_status = self.setup_monitor(model_apps)
+            if return_status is False:
+                del model_apps
+                return
 
-        setup_button.clicked.connect(lambda: self.setup_tile(model_apps))
+            model_apps.image_result.connect(lambda img: self.update_label_image(label, img))
+            model_apps.create_image_result()
 
-        # to make the model_apps instance alive
-        self.idx2model[n] = model_apps
-        self.model2idx[model_apps] = n
-        self.cur_idx = (self.cur_idx + 1) % 9 
-        if self.cur_idx == 0: 
-            self.cur_idx += 1
-        print(self.cur_idx)
+            setup_button.clicked.connect(lambda: self.setup_monitor(model_apps))
+            delete_button.clicked.connect(lambda: self.delete_monitor(model_apps))
 
-    def update_label_image(self, image, ui_label, width=300, scale_content=False):
+            # to keep the model_apps instance alive
+            self.grid_manager.set_slot(ui_idx, model_apps)
+
+        
+
+    def update_label_image(self, ui_label, image, width=300, scale_content=False):
         self.model.show_image_to_label(ui_label, image, width=width, scale_content=scale_content)
 
-    def setup_tile(self, model_apps: ModelApps):
+    def setup_monitor(self, model_apps: ModelApps):
         ui_setup = Ui_Setup()
         dialog = SetupDialog()
+
         ui_setup.setupUi(dialog)
+        ui_setup.cancelButton.clicked.connect(dialog.close_function)
+        ui_setup.okButton.clicked.connect(dialog.accept_function)
 
         # setup and gracefully close the slots and signals of image_result and signal_image_original from ModelApps
-        update_result_label_slot = lambda img: self.update_label_image(img, ui_setup.label_image_result, 320, False)
+        update_result_label_slot = lambda img: self.update_label_image(ui_setup.label_image_result, img, 320, False)
         dialog.setup_result_signal(update_result_label_slot, model_apps.image_result)
-        update_original_label_slot = lambda img: self.update_label_image(img, ui_setup.label_image_original, 320, False)
+        update_original_label_slot = lambda img: self.update_label_image(ui_setup.label_image_original, img, 320, False)
         dialog.setup_original_signal(update_original_label_slot, model_apps.signal_image_original)
 
         model_apps.alpha_beta.connect(self.alpha_beta_from_coordinate)
@@ -133,7 +179,48 @@ class Controller(QtWidgets.QWidget):
         # ui_setup.label_image_original.mouseDoubleClickEvent =
 
         # start setup dialog
-        dialog.exec()
+        result = dialog.exec()
+        if result == QtWidgets.QDialog.DialogCode.Accepted:
+            return True
+        elif result == QtWidgets.QDialog.DialogCode.Rejected:
+            return False
+
+    def delete_monitor(self, model_apps):
+        ui_idx = self.grid_manager.get_index_of_slot(model_apps)
+        if ui_idx == -1: return
+        
+        label, setup_button, capture_button, _ = self.get_monitor_ui_by_idx(ui_idx)
+        label.setText(' ')
+        setup_button.clicked.disconnect()
+        # capture_button.clicked.disconnect()
+    
+        model_apps.timer.stop()
+        model_apps.__image_result = None
+        model_apps.image = None
+        model_apps.image_resize = None
+   
+        model_apps.reset_config()
+    
+        if model_apps.cap is not None:
+            try:
+                model_apps.cap.close()
+            except:
+                pass
+        model_apps.cap = None
+
+        # THIS WILL MAKE "QThread: Destroyed while thread is still running"
+        # Already used QTimer.singleShot(1000, lambda: ...) but still same results
+        # delete_button.clicked.disconnect() 
+
+        self.grid_manager.clear_slot(ui_idx)
+        
+
+    def get_monitor_ui_by_idx(self, ui_idx):
+        label = getattr(self.ui, "displayLab%s" % ui_idx)
+        setup_button = getattr(self.ui, "setupButton%s" % ui_idx)
+        capture_button = getattr(self.ui, "captureButton%s" % ui_idx)
+        delete_button = getattr(self.ui, "deleteButton%s" % ui_idx)
+        return label, setup_button, capture_button, delete_button
 
     def alpha_beta_from_coordinate(self, alpha_beta):
         print(alpha_beta)
@@ -149,9 +236,6 @@ class Controller(QtWidgets.QWidget):
 
     def recorded_clicked(self):
         print('recorded_clicked')
-
-    # def __tileLayoutResize(self, a0):
-    #     self.tile_layout.updateGlobalSize(a0)
 
 
 class Surveillance(PluginInterface):
